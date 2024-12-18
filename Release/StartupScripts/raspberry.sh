b62ae7e331572158769c9377ee99500fe0625609
invpe <106522950+invpe@users.noreply.github.com>
2024-12-18 16:35:43 +0100

    Update raspberry.sh

--- Release/StartupScripts/raspberry.sh
+++ Release/StartupScripts/raspberry.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Wait for the network to be up
-sleep 60
 
 # Define the path to the home directory dynamically
 USER_HOME="$HOME"
@@ -15,19 +13,35 @@ fi
 SERVER="$1"
 NODE_ID="$2"
 
+# Wait for the network to be up
+sleep 60
+
+# Detect architecture
+ARCH=$(uname -m)
+if [[ "$ARCH" == "aarch64" ]]; then
+    BINARY_URL="https://github.com/invpe/HashNet/releases/latest/download/hashnet_arm64.bin"
+    BINARY_NAME="hashnet_arm64.bin"
+elif [[ "$ARCH" == "armv7l" || "$ARCH" == "armhf" ]]; then
+    BINARY_URL="https://github.com/invpe/HashNet/releases/latest/download/hashnet_arm32.bin"
+    BINARY_NAME="hashnet_arm32.bin"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
 # Remove the old binary
-rm -f "$USER_HOME/hashnet_arm64.bin"
+rm -f "$USER_HOME/$BINARY_NAME"
 
 # Download the latest binary
-wget https://github.com/invpe/HashNet/releases/latest/download/hashnet_arm64.bin -O "$USER_HOME/hashnet_arm64.bin"
+wget "$BINARY_URL" -O "$USER_HOME/$BINARY_NAME"
 
 # Make the binary executable
-chmod +x "$USER_HOME/hashnet_arm64.bin"
+chmod +x "$USER_HOME/$BINARY_NAME"
 
 # Get the number of CPU cores
 CORES=$(nproc)
 
 # Start one instance per core in separate screen sessions
 for ((i=0; i<CORES; i++)); do
-    screen -dmS hashnet_runner_$i bash -c "cd $USER_HOME; while true; do ./hashnet_arm64.bin $SERVER $NODE_ID; done"
+    screen -dmS hashnet_runner_$i bash -c "cd $USER_HOME; while true; do ./$BINARY_NAME $SERVER $NODE_ID; done"
 done
